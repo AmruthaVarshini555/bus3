d1062f421ecd89aa3b95c9217cb1129ddb0c186b
AmruthaVarshini555 <119595744+AmruthaVarshini555@users.noreply.github.com>
2023-01-19 13:13:41 +0530

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include<string>
+#include<cstring>
+#include<fstream>
+#include"menu.h"
+
+using namespace std;
+
+int main()
+{
+    Menu display;
+    display.welcomeScreen();
+    return EXIT_SUCCESS;
+}
